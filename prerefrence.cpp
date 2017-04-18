3d12876fa81f6cfcd5a4da8e9cf127f91d77f79c
antipro <antipro@163.com>
2017-04-18 18:28:22 +0800

    修改界面

--- prerefrence.cpp
+++ prerefrence.cpp
@@ -6,10 +6,30 @@ Prerefrence::Prerefrence(QWidget *parent) :
     ui(new Ui::Prerefrence)
 {
     ui->setupUi(this);
+    QSettings *config = new QSettings("config.ini", QSettings::IniFormat);
+    int sizePolicy = config->value("size_policy", QVariant(0)).toInt();
+    switch(sizePolicy){
+    case 0:
+        ui->radio1->setChecked(true);break;
+    case 1:
+        ui->radio2->setChecked(true);break;
+    case 2:
+        ui->radio3->setChecked(true);
+    }
+
+    int maxSize = config->value("max_size", QVariant(300)).toInt();
+    ui->spinBox->setValue(maxSize);
 }
 
 void Prerefrence::accept(){
-    bool sizePolicy =  ui->checkBox->isChecked();
+    int sizePolicy = 0;
+    if(ui->radio1->isChecked())
+        sizePolicy = 0;
+    if(ui->radio2->isChecked())
+        sizePolicy = 1;
+    if(ui->radio3->isChecked())
+        sizePolicy = 3;
+
     int maxSize = ui->spinBox->value();
     QSettings *config = new QSettings("config.ini", QSettings::IniFormat);
     if(config->isWritable()){
